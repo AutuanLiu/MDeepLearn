a38fa1d7cab47b7be20460feff90868d5c97b44b
AutuanLiu <autuanliu@163.com>
2017-12-20 11:03:11 +0800

    add config

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-curl -fsSL get.docker.com -o get-docker.sh
-sudo sh get-docker.sh --mirror Aliyun
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -aG docker $USER
-
 docker build -t ml-example:latest -f Dockerfile .
