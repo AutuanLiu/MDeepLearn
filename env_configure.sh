0f408d1348773c78efb3bd5d47568c4e2703a2c1
AutuanLiu <autuanliu@163.com>
2018-01-06 10:14:09 +0800

    env shell scripts

--- env_configure.sh
+++ env_configure.sh
@@ -9,9 +9,9 @@
 # sudo ./env_configure.sh
 
 # MiniConda install
-export PATH=/opt/conda/bin:$PATH > /etc/profile.d/conda.sh &&
 wget -nv https://repo.continuum.io/miniconda/Miniconda3-4.3.27-Linux-x86_64.sh -O ~/anaconda.sh &&
 /bin/bash ~/anaconda.sh -b -p /opt/conda && rm ~/anaconda.sh &&
+export PATH=/opt/conda/bin:$PATH > /etc/profile.d/conda.sh &&
 
 # channel set
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/ &&
@@ -19,13 +19,13 @@ conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/f
 conda config --set show_channel_urls yes &&
 
 # pip3 install
-python3 -m pip install --upgrade pip &&
+python -m pip install --upgrade pip &&
 
 # opencv
-apt-get -qq install -y libsm6 libxext6 && pip3 install -q -U opencv-python &&
+apt-get -qq install -y libsm6 libxext6 && pip install -q -U opencv-python &&
 
 # graphviz
-apt-get -qq install -y graphviz && pip3 install -q pydot &&
+apt-get -qq install -y graphviz && pip install -q pydot &&
 
 # conda pkgs install
 conda install -y pillow scipy &&
@@ -44,7 +44,10 @@ conda install -y r-nycflights13=0.2* r-caret=6.0* r-rcurl=1.95* r-crayon=1.3* r-
 conda clean -tipsy &&
 
 # pip pkgs install
-pip3 install -r pip_pkgs.txt &&
+pip install -r pip_pkgs.txt &&
 
 # jupyter notebook
-python3 -m pip install jupyter
+python -m pip install jupyter &&
+
+# add to path
+export PATH=/opt/conda/bin:$PATH && bash
