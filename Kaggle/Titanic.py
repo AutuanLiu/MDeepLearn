046f3476bc971f45cfb295c1e544f7b530da852a
AutuanLiu <autuanliu@163.com>
2018-01-07 11:33:54 +0800

    kaggele prepare

--- Kaggle/Titanic.py
+++ Kaggle/Titanic.py
@@ -4,7 +4,32 @@
 """
 -------------------------------------------------
    File Name：Titanic
-   Description :  
+   Description : Titanic 预测 
    Email : autuanliu@163.com
    Date：2017/12/29
 """
+
+class Titanic:
+    def __init__(self, *args):
+        pass
+    
+    def load_data(self, *args):
+        pass
+
+    def model(self, *args):
+        pass
+    
+    def fit_f(self, *args):
+        pass
+
+    def predict(self, *args):
+        pass
+
+    def run(self, *args):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
