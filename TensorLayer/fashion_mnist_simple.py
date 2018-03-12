976ddf61b7610d4fcec8c628bfd2e88cfbb3bcec
AutuanLiu <autuanliu@163.com>
2018-03-12 19:56:43 +0800

    update mnist

---
+++ TensorLayer/fashion_mnist_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name： fashion_mnist_simple
+   Description :  
+   Email : autuanliu@163.com
+   Date： 18-3-12
+"""
