fbcb80136dcc977ce59195fccebd04a752b7ee21
AutuanLiu <autuanliu@163.com>
2018-04-08 21:40:59 +0800

    update

--- Pytorch/Antsbees.py
+++ Pytorch/Antsbees.py
@@ -252,7 +252,7 @@ optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=8, gamma=0.1)
 
 # Train and evaluate
-model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=1)
+model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=5)
 visualize_model(model_conv)
 # 仅保存和加载模型参数(推荐使用)
 torch.save(model_conv.state_dict(), 'trans_params.pkl')
